062dce42f630deeb11c2f143634275547b80e791
Artem <40913z@mail.ru>
2021-01-23 01:55:02 +0300

    developed a prototype of the future server

---
+++ cloud-server/src/main/java/com/temzu/cloud_storage/server/NettyNetwork.java
@@ -0,0 +1,50 @@
+package com.temzu.cloud_storage.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
+
+public class NettyNetwork {
+
+    private final int PORT;
+    private Path rootFolder;
+
+    private static final Logger LOG = LoggerFactory.getLogger(NettyNetwork.class);
+
+    public NettyNetwork(int PORT, Path rootFolder) {
+        this.PORT = PORT;
+        this.rootFolder = rootFolder;
+    }
+
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyClientHandler(rootFolder));
+                        }
+                    });
+            ChannelFuture future = b.bind(PORT).sync();
+            LOG.debug("Server started on PORT = " + PORT);
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            LOG.error("e=", e);
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ cloud-server/src/main/java/com/temzu/cloud_storage/server/ServerApp.java
@@ -0,0 +1,23 @@
+package com.temzu.cloud_storage.server;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ServerApp {
+
+    private static final int PORT = 8189;
+    public static Path rootFolder;
+
+    static {
+        try {
+            rootFolder = Paths.get(ServerApp.class.getResource("/root_folder").toURI());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NettyNetwork(PORT, rootFolder).run();
+    }
+}
