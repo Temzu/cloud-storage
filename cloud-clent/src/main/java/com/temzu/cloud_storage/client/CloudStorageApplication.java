ff2ce4c3289ccb8cbf670aeba850aad94db7809c
Artem <40913z@mail.ru>
2021-01-22 23:53:38 +0300

    made an example of future design

---
+++ cloud-clent/src/main/java/com/temzu/cloud_storage/client/CloudStorageApplication.java
@@ -0,0 +1,21 @@
+package com.temzu.cloud_storage.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CloudStorageApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/CloudStorageLayout.fxml"));
+        primaryStage.setTitle("Cloud storage");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ cloud-clent/src/main/java/com/temzu/cloud_storage/client/CloudStorageController.java
@@ -0,0 +1,35 @@
+package com.temzu.cloud_storage.client;
+
+import javafx.event.ActionEvent;
+
+public class CloudStorageController {
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRefreshOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDownloadOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUploadOnAction(ActionEvent actionEvent) {
+    }
+
+    public void selectDiskOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPathUpAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteClientOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRefreshClientOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteCloudOnAction(ActionEvent actionEvent) {
+    }
+}
